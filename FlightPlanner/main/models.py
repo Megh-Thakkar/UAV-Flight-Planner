0d7a274096fcef3992aff307ec020ef787c7de0d
Megh-Thakkar <megh.1211@gmail.com>
2018-06-05 11:16:07 +0530

    Compiled the functions:

--- FlightPlanner/main/models.py
+++ FlightPlanner/main/models.py
@@ -7,7 +7,9 @@ from django.db import models
 class KMLFile(models.Model):
     file_path = models.FileField(upload_to='files/', null=True)
     name = models.CharField(max_length=20)
-
+    csv_file = models.FileField(upload_to='csv/', null=True)
+    zone_no = models.IntegerField(default=0)
+    zone_name = models.CharField(max_length=2, null=True)
     def __unicode__(self):
         return self.name
 
--- FlightPlanner/main/views.py
+++ FlightPlanner/main/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 from .models import *
-import utm
+from django.conf import settings
+import os
 # Create your views here.
 
 def upload_file(request):
@@ -25,6 +26,7 @@ def test(request):
 
 def input_map(request):
     if request.method == 'POST':
+        import utm
         data = request.POST
         if (data['ne_lng'] == '' or data['ne_lat'] == '' or data['sw_lng'] == '' or data['sw_lat'] == ''):
             return redirect("main:input_map")
@@ -32,56 +34,66 @@ def input_map(request):
         ne_lat = float(data['ne_lat'])
         sw_lng = float(data['sw_lng'])
         sw_lat = float(data['sw_lat'])
-
-        import utm
-        NE_UTM = utm.from_latlon(ne_lat, ne_lng)
-        SW_UTM = utm.from_latlon(sw_lat, sw_lng)
-        print NE_UTM, SW_UTM
-        return HttpResponse('Coords printed')
+        x_resolution = int(data['x_res'])
+        y_resolution = int(data['y_res'])
+        GSD = float(data['gsd'])
+        NE_UTM_X, NE_UTM_Y, NE_UTM_ZONE_NO, NE_UTM_NAME = utm.from_latlon(ne_lat, ne_lng)
+        SW_UTM_X, SW_UTM_Y, SW_UTM_ZONE_NO, SW_UTM_NAME = utm.from_latlon(sw_lat, sw_lng)
+        if(NE_UTM_NAME!=SW_UTM_NAME or NE_UTM_ZONE_NO!=SW_UTM_ZONE_NO):
+            return HttpResponse("UTM Zones do not match. Select a smaller region.")
+        else:
+            return redirect(reverse("main:generate_csv", kwargs={"x_resolution":x_resolution, "y_resolution":y_resolution, "x1":NE_UTM_X, "y1":NE_UTM_Y, "x2":SW_UTM_X, "y3":SW_UTM_Y, "GSD":GSD, "zone_no":NE_UTM_ZONE_NO, "zone_name":NE_UTM_NAME}))
     else:
         return render(request, 'main/input_map.html')
 
+def generate_csv(request, x_resolution, y_resolution, x1, y1, x2, y3,  GSD, zone_no, zone_name):
+    line_path = pathline(mesh(x_resolution, y_resolution, x1, y1, x2, y3,  GSD, pixel_to_km=0.00001))
+    i=0
+    while(KMLFile.objects.get(name=i)):
+        i += 1
+    path = os.path.join(settings.MEDIA_ROOT, 'csv', str(i)+'.csv')
+    with open(path, 'wb') as csvfile:
+        csvfile.truncate()
+        filewriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+        filewriter.writerow('X', 'Y')
+        for i in range(0, len(line_path)):
+            filewriter.writerow([str(line_path[i]['X']), str(line_path[i]['Y'])])
+    kml_file = KMLFile()
+    kml_file.name = str(i)
+    kml_file.csv_file.name = path
+    kml_file.zone_name = zone_name
+    kml_file.zone_no = zone_no
+    kml_file.save()
+    generate_kml(path, kml_file)
+    return render(request, 'main/test.html', {'name':kml_file+'.kml'})
 ####################################    Main Functions    ####################################
 
 import csv
 import simplekml
+import numpy as np
 
-
-def get_grid_list(x_resolution, y_resolution, x1, y1, x2, y2, x3, y3, x4, y4, GSD, pixel_to_km=0.00001, img_overlap=0.2):
-    coordinates = []
-    per_X = GSD * x_resolution * pixel_to_km
-    per_Y = GSD * y_resolution * pixel_to_km
-    y = y1
-    while (y <= y3):
-        x = x4
-        while (x <= x2):
-            coordinates.append({'X':x, 'Y':y})
-            x = x - img_overlap + per_X
-        y = y - img_overlap + per_Y
-
-
-def generate_kml(filename, zone_no, zone_name):
+def generate_kml(filename, kml_file):
     import utm
     inputfile = csv.reader(open(filename,'r'))
     kml=simplekml.Kml()
     ls = kml.newlinestring(name="Journey path")
-
+    zone_no = kml_file.zone_no
+    zone_name = kml_file.zone_name
     inputfile.next()
     for row in inputfile:
         lat, lng = utm.to_latlon(row[0], row[1], zone_no, zone_name)
-        ls.coords.addcoordinates([(lng, lat, row[2])])  #longitude, latitude
-        print row[2]
+        ls.coords.addcoordinates([(lng, lat, get_elevation(lat, lng))])  #longitude, latitude
+        # print row[2]
     ls.extrude = 1
     ls.tessellate = 1
     ls.altitudemode = simplekml.AltitudeMode.absolute
     ls.style.linestyle.color = '7f00ffff'   #aabbggrr
     ls.style.linestyle.width = 4
     ls.style.polystyle.color = '7f00ff00'
-    kml.save('fooline.kml')
-
-
-
-######################## changes and new fnc ###########
+    kml_path = os.path.join(settings.MEDIA_ROOT, 'files', str(kml_file.name)+'.kml')
+    kml.save(kml_path)
+    kml_file.file_path.name = kml_path
+    kml_file.save()
 
 def get_elevation(lat, lng):
     import urllib, json
@@ -89,16 +101,12 @@ def get_elevation(lat, lng):
                             str(lng) + "&key=AIzaSyDTJkkx8M1hzY3OpG-lL66LmoBYoZRKMBg")
     return float(json.load(response)["results"][0]["elevation"])
 
-
-centres = []
-import numpy as np
-
-
-def mesh(x_resolution, y_resolution, x1, y1, x2, y2, x3, y3, x4, y4, GSD, pixel_to_km=0.00001):
-    per_X = GSD * x_resolution * pixel_to_km
-    per_Y = GSD * y_resolution * pixel_to_km
-    lx = np.linspace(0, x2, int((x2 - x1) / per_X))
-    ly = np.linspace(0, y3, int((y3 - y1) / per_Y))
+def mesh(x_resolution, y_resolution, x1, y1, x2, y3, GSD, pixel_to_km=0.00001):
+    centres = []
+    per_X = GSD * x_resolution * pixel_to_km                        ### x3,y3-------x4,y4
+    per_Y = GSD * y_resolution * pixel_to_km                        #    |            |
+    lx = np.linspace(0, x2, int((x2 - x1) / per_X))                 #    |            |
+    ly = np.linspace(0, y3, int((y3 - y1) / per_Y))                 ### x1,y1-------x2,y2
     kx, ky = np.meshgrid(lx, ly)
 
     for i in range(0, len(kx) - 1):
@@ -107,8 +115,9 @@ def mesh(x_resolution, y_resolution, x1, y1, x2, y2, x3, y3, x4, y4, GSD, pixel_
             centre = (kx[i][j] + kx[i][j + 1]) / 2.00
             centres.append({'X': centre, 'Y': y})
 
+    return centres
 
-def pathline(centres=[]):
+def pathline(centres):
     path = []
     lineno = 0
     rev = []
@@ -131,19 +140,7 @@ def pathline(centres=[]):
             lineno += 1
     return path
 
-
-import csv
-
-
-def cfile(centres=[]):
-    with open('points.csv', 'wb') as csvfile:
-        filewriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-        filewriter.writerow('X', 'Y')
-        for i in range(0, len(centres)):
-            filewriter.writerow([str(centres[i]['X']), str(centres[i]['Y'])])
-
-
-"""mesh(1000, 1000, 0, 0, 1, 0, 1, 2, 0, 2, 5)
+"""centres = mesh(1000, 1000, 0, 0, 1, 0, 1, 2, 0, 2, 5)
 path = pathline(centres)
 cfile(centres)
 # for i in range(0,len(path)):
